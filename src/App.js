03875f9997bdb4b9ad34fc9986a366c5faa4fdb5
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-19 11:52:45 -0500

    Sets up router and provider for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import triviaReducer from './reducers';
+
+const store = createStore(triviaReducer);
+window.store = store;
 
 function App() {
   return (
-    <div className="App-header">
+    <Provider store={store}>
+      <Router>
 
-    </div>
+      </Router>
+    </Provider>
   );
 }
 
