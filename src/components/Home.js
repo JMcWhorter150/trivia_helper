eaa75f5cfc9a38ae8a333283b7f5b94a16d79576
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-21 14:08:27 -0500

    Adds picture to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,13 @@ import React from 'react';
 export default function Home() {
     return (
         <div id="home">
-            <div className="pictureFrame">
-                <img src={process.env.PUBLIC_URL + '/study.png'} alt="logo" />
-            </div>
-            <h1>Trivia Study Guide</h1>
-            <h2>Your guide to acing trivia</h2>
+            <div id="overlay">
+                <h1>Trivia Study Guide</h1>
+                <h2>Your guide to acing trivia</h2>
 
-            <p>Question of the day:</p>
-            <p>Hidden Answer</p>
+                <p>Question of the day:</p>
+                <p>Hidden Answer</p>
+            </div>
         </div>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom';
 export default function Nav() {
     return (
         <ul id="nav">
+            <div className="pictureFrame">
+                <img src={process.env.PUBLIC_URL + '/study.png'} alt="logo" />
+            </div>
             <Link to="/">Home</Link>
             <Link to="/today">Today in History</Link>
             <Link to="/news">News</Link>
