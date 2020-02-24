821ac37bad3534ddb088e7c0feaab243272a33fa
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-24 12:12:56 -0500

    adds button changes on news page

--- src/components/CurrentEventsList.js
+++ src/components/CurrentEventsList.js
@@ -6,7 +6,18 @@ export default class RecentSports extends React.Component {
 
         this.state = {
             topic: "",
-            optionsList: ['currentNews', 'sportsNews']
+            optionsList: ['currentNews', 'sportsNews'],
+            saved: [...this.props.saved]
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.saved !== state.saved) {
+            return {
+                saved: props.saved
+            }
+        } else {
+            return state;
         }
     }
 
@@ -21,14 +32,16 @@ export default class RecentSports extends React.Component {
     }
 
     render() {
-        const {handleSelect} = this.props;
+        const {handleSelect, saved} = this.props;
         const {topic} = this.state;
-        const newsList = topic ? this.props[topic].map((item, i) => (
-            <li key={i + "E"} className="trivia">
-                <button onClick={() => handleSelect(item.title)}>Save</button>
-                <a href={item.url} rel='noopener noreferrer' target="_blank"> {item.title}</a>
-            </li>
-        )) : "";
+        const newsList = topic ? this.props[topic].map((item, i) => {
+            let buttonText = saved.includes(item.title) ? "Saved" : "Save";
+            return (
+                <li key={i + "E"} className="trivia">
+                    <button className={buttonText} onClick={() => {handleSelect(item.title); this._updateSaved(item.title)}}>{buttonText}</button>
+                    <a href={item.url} rel='noopener noreferrer' target="_blank"> {item.title}</a>
+                </li>
+        )}) : "";
 
         let optionsList = this.state.optionsList.map((text, i) => {
             let displayText = text === "sportsNews" ? "Sports" : text === "currentNews" ? "U.S." : "";
@@ -58,4 +71,9 @@ export default class RecentSports extends React.Component {
         })
     }
     
+    _updateSaved = (text) => {
+        this.setState({
+            saved: [...this.state.saved, text]
+        })
+    }
 }
